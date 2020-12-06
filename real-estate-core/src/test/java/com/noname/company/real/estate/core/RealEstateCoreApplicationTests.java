870866b67a7727eef2090f9c480b2239649f090f
avahiel <avahiel59@gmail.com>
2020-12-06 15:45:59 +0100

    project core init

---
+++ real-estate-core/src/test/java/com/noname/company/real/estate/core/RealEstateCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.company.real.estate.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
